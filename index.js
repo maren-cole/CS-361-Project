217c2196a27243f9595487afd37ec08adbd34baa
maren-cole <marcole2013@gmail.com>
2024-06-04 19:48:12 -0700

    add gallery page

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 9000;
 
-app.use(express.static('public'));
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:6000', 'http://localhost:3001'],
+  optionsSuccessStatus: 200,
+};
+
+app.use(express.static('public'), cors(corsOptions));
 
 // Start listening on the specified PORT
 app.listen(PORT, () => {
